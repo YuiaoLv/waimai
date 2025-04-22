c513db508aeb10a8cc59befe06c2846512deb0c2
lxy <2746332296@qq.com>
2025-04-22 10:49:24 +0800

    完成新增菜品及口味

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,7 +1,13 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -14,4 +20,17 @@ public interface DishMapper {
     @Select("select count(id) from  sky_take_out.dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+
+    /**
+     * 新增菜品
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
+
+    /**
+     * 新增菜品口味数据
+     * @param flavor
+     */
+    void insertFlavor(List<DishFlavor> flavor);
 }
