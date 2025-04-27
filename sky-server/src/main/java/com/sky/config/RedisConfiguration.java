abc230a81bf748a81352f74caa6bfb47747ea8c9
lxy <2746332296@qq.com>
2025-04-27 14:35:54 +0800

    (看似啥都没改，但终于把ubuntu上的redis配置好了

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -16,8 +16,12 @@ public class RedisConfiguration {
         log.info("开始创建redisTemplate对象...");
         RedisTemplate redisTemplate = new RedisTemplate();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        // 设置key的序列化器
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 设置key的序列化器 解决乱码
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 设置value的序列化器 解决乱码
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        // 直接设置默认的序列化器
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
